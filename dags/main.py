88e611e1534ab891e287873c4dbe733aca7e974c
Youssef Jebroun <jebronyoussef@gmail.com>
2026-04-16 18:30:49 +0200

    adding unit tests using pytest

--- dags/main.py
+++ dags/main.py
@@ -40,15 +40,22 @@ with DAG(
     description="DAG to produce json file with raw data",
     schedule="0 19 * * *",
     catchup=False,
-) as dag :
+) as dag_produce :
 
     playlistId = get_playlist_id()
     video_ids = get_video_id(playlistId)
     video_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(video_data)
 
-    playlistId >> video_ids >> video_data >> save_to_json_task
+    #playlistId >> video_ids >> video_data >> save_to_json_task
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
+    # Define dependencies
+    playlistId >> video_ids >> video_data >> save_to_json_task >> trigger_update_db
 
 
 with DAG(
@@ -56,7 +63,7 @@ with DAG(
     default_args=default_args,
     description="DAG to process JSON file and insert data into both staging and core schemas",
     catchup=False,
-    schedule="0 20 * * *",
+    schedule=None,
 ) as dag_update:
 
     # Define tasks
@@ -64,8 +71,13 @@ with DAG(
     update_core = core_table()
 
     # Define dependencies
-    update_staging >> update_core 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
+    # Define dependencies
+    update_staging >> update_core >> trigger_data_quality
 
 # DAG 3: data_quality
 with DAG(
